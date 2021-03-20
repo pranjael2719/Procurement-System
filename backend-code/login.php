2c2447b4a503cac956d51f58683b9ea2b957edd3
InsideU <umang20anku@gmail.com>
2021-03-20 17:20:00 +0530

    Login update

--- backend-code/login.php
+++ backend-code/login.php
@@ -4,25 +4,46 @@
         header("location:welcome.php");
         exit;
     }
-$database = procurement_db;
 require_once "config.php";
     $myusername = $mypassword = "";
+    $count=0;
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         $myusername = $_POST['username'];
         $mypassword = $_POST['password'];
+        $role = $_POST['category'];
+        if($role =="company"){
+            $sql = "SELECT id FROM $company  WHERE Username ='$myusername' and Password='$mypassword' ";
+            $result = mysqli_query($db,$sql);
+            $count = mysqli_num_rows($result);
+            if($count ==1){
+                session_register("myusername");
+                $_SESSION['login_user'] = $myusername;
+                header("location: company.php");
     
-        $sql = "SELECT id FROM $database  WHERE Username ='$myusername' and Password='$mypassword' ";
-        $result = mysqli_query($db,$sql);
-        $count = mysqli_num_rows($result);
-        if($count ==1){
-            session_register("myusername");
-            $_SESSION['login_user'] = $username;
-            header("location: welcome.php");
-
+            }
+            else{
+                $error = "Invalid Password";
+                echo $error;
+            }
+        }
+        else if($role=="vendor"){
+            $sql = "SELECT id FROM $vendor  WHERE Username ='$myusername' and Password='$mypassword' ";
+            $result = mysqli_query($db,$sql);
+            $count = mysqli_num_rows($result);
+            if($count ==1){
+                session_register("myusername");
+                $_SESSION['login_user'] = $myusername;
+                header("location: vendor.php");
+    
+            }
+            else{
+                $error = "Invalid Password";
+                echo $error;
+            }
         }
         else{
-            $error = "Invalid Password";
-            echo $error;
+            echo "Please select the category";
         }
+       
     }
 ?>
