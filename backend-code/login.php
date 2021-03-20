d5f8e0fd52e01f0c1a12d93c7ecd03f7c3a7ddc8
InsideU <umang20anku@gmail.com>
2021-03-20 17:03:44 +0530

    loing page created

---
+++ backend-code/login.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    if(isset($SESSION['login'] && $SESSION['login']==true)){
+        header("location:welcome.php");
+        exit;
+    }
+$database = procurement_db;
+require_once "config.php";
+    $username = $password = "";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $myusername = $_POST['username'];
+        $mypassword = $_POST['password'];
+    
+        $sql = "SELECT id FROM $database  WHERE Username ='$myusername' and Password='$mypassword' ";
+        $result = mysqli_query($db,$sql);
+        $count = mysqli_num_rows($result);
+        if($count ==1){
+            session_register("myusername");
+            $_SESSION['login_user'] = $username;
+            header("location: welcome.php");
+
+        }
+        else{
+            $error = "Invalid Password";
+            echo $error;
+        }
+    }
+?>
