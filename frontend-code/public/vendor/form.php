adfe5a0a3724012c384e311bf095816d176248cb
InsideU <umang20anku@gmail.com>
2021-03-20 23:35:30 +0530

    changed the form.php file

--- frontend-code/public/vendor/form.php
+++ frontend-code/public/vendor/form.php
@@ -4,11 +4,6 @@
         <link rel="stylesheet" href="form.css">
     </head>
     <body>
-<<<<<<< HEAD:frontend-code/public/vendor/form.php
-        <div class="container">
-            <div class="form-style-2">
-                <form action="../../../backend-code/form_submit.php" method="post">
-=======
         <div class="form-design">
             <h1> Proposal Form </h1>
                 <form action="" method="post">
@@ -20,7 +15,6 @@
                     </div>
                     <div class="sec"><span>2</span>Fill up</div>
                     <div class="box">
->>>>>>> ea7969fd3dad54c4625a835adc2b1b1a172e85c7:frontend-code/public/vendor/form.html
                     <label for="field1"><span>Cost per unit <span class="required">*</span></span><input type="number" class="input-field" name="field1" value="" /></label>
                     <label for="field2"><span>Agreement start date <span class="required">*</span></span><input type="date" class="input-field" name="field2" value="" /></label>
                     <label for="field3"><span>Agreement end date </span><input type="date" class="input-field" name="field3" value="" /></label>
