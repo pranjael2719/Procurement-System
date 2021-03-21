bc7050fd39659a477bac330202274d9498944612
InsideU <umang20anku@gmail.com>
2021-03-21 09:22:33 +0530

    changed the negotitation cannot be changed after accepted done

--- backend-code/changecost.php
+++ backend-code/changecost.php
@@ -2,7 +2,19 @@
 require_once "config.php";
 $res_id = $_GET['res_id'];
 $cost= $_POST['cost'];
+$sql1 = "SELECT Status FROM status WHERE Response_ID='$res_id'";
+$result = mysqli_query($db,$sql1);
+$row = mysqli_fetch_assoc($result);
+
+$currentStatus = $row['Status'];
+
+if($currentStatus!="Accepted"){
 $sql = "UPDATE rfp_status SET Cost='$cost' WHERE Response_ID='$res_id'";
 mysqli_query($db,$sql);
 header("location: ../frontend-code/public/vendor/rfpstatus.php");
+}
+else{
+    header("location: ../frontend-code/public/vendor/rfpstatus.php");
+
+}
 ?>
